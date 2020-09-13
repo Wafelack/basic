e351189dacbee9a80400b352b6e2ae2b9630bd67
Wafelack <wafelack@protonmail.com>
2020-09-13 17:48:50 +0200

    updating all

--- bot.py
+++ bot.py
@@ -11,36 +11,10 @@ PREFIX = '!'
 WAFEID = 723862906755743804
 
 
-async def show_bitcoin(chan):
-    while True:
-        VALUES = json.loads(urllib.request.urlopen('https://blockchain.info/ticker').read().decode())
-        buy_usd = VALUES['USD']['buy']
-        sell_usd = VALUES['USD']['sell']
-
-        buy_eur = VALUES['EUR']['buy']
-        sell_eur = VALUES['EUR']['sell']
-
-        buy_rub = VALUES['RUB']['buy']
-        sell_rub = VALUES['RUB']['sell']
-
-        embed = discord.Embed(title="BTC Value:",
-                              description=f"**USD :**\n\tSell : ${sell_usd}\n\tBuy: ${buy_usd}\n\n**EUR :**\n\tSell : {sell_eur}€\n\tBuy : {buy_eur}€\n\n**RUB :**\n\tBuy : {buy_rub} Pуб\n\tSell : {sell_rub} Pуб",
-                              color=0xffbb00)
-        embed.set_footer(text="Data picked on https://blockchain.info/ticker")
-
-        await chan.send(embed=embed)
-        await asyncio.sleep(1800)
-
-
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(name='!playground', type=discord.ActivityType.playing))
     print("FerriBot is ready")
-    BOTS = client.get_channel(743853002523148349)
-    client.loop.create_task(
-        show_bitcoin(BOTS)
-    )
-
-
 @client.event
 async def on_message(message):
     if message.content.startswith(PREFIX + 'get_crate'):
@@ -49,9 +23,7 @@ async def on_message(message):
             await message.channel.send(f"Usage : {PREFIX}get_crate <crate>")
             return
         embed = discord.Embed(
-            title=f'{splited[1]}',
-            description=f'docs.rs : https://docs.rs/{splited[1]}\ncrates.io : https://crates.io/crates/{splited[1]}',
-            color=00)
+            title=f'{splited[1]}', description=f'docs.rs : https://docs.rs/{splited[1]}\ncrates.io : https://crates.io/crates/{splited[1]}', color=00)
         await message.channel.send(f"<@{message.author.id}>\n", embed=embed)
     if message.content.startswith(PREFIX + 'playground'):
         splited = message.content.split('\n')
@@ -66,6 +38,21 @@ async def on_message(message):
     if message.content == PREFIX + "make_me_rustacean":
         await message.author.add_roles(message.author.guild.get_role(743864011334090762))
         await message.channel.send("Le rôle vous a été donné avec succès !")
+    if message.content == PREFIX + "btc":
+        VALUES = json.loads(urllib.request.urlopen('https://blockchain.info/ticker').read().decode())
+        buy_usd = VALUES['USD']['buy']
+        sell_usd = VALUES['USD']['sell']
+
+        buy_eur = VALUES['EUR']['buy']
+        sell_eur = VALUES['EUR']['sell']
+
+        buy_rub = VALUES['RUB']['buy']
+        sell_rub = VALUES['RUB']['sell']
+
+        embed = discord.Embed(title="BTC Value:", description=f"**USD :**\n\tSell : ${sell_usd}\n\tBuy: ${buy_usd}\n\n**EUR :**\n\tSell : {sell_eur}€\n\tBuy : {buy_eur}€\n\n**RUB :**\n\tBuy : {buy_rub} Pуб\n\tSell : {sell_rub} Pуб", color=0xffbb00)
+        embed.set_footer(text="Data picked on https://blockchain.info/ticker")
+
+        await message.channel.send(embed=embed)
 
     if message.content == PREFIX + "help":
         embed = discord.Embed(title="Rustacean",
